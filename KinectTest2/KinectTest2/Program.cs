9a2b03e065d11aac7e106bb858099d275660ca37
Ruhi <Ruhi>
2017-02-04 22:17:33 +0000

    gets DepthFrames and calls dummy method with 2d array

--- KinectTest2/KinectTest2/Program.cs
+++ KinectTest2/KinectTest2/Program.cs
@@ -8,52 +8,106 @@ using Microsoft.Kinect.Face;
 using Microsoft.Kinect.Fusion;
 using Microsoft.Kinect.Tools;
 
+
 namespace KinectTest2
 {
     class Run
     {
-        public KinectSensor sensor = null;
-        public DepthFrameReader depthFrameReader = null;
-        private ushort[] depthFrameData = null;
-        private byte[] depthPixels = null
+        private static void Dummy(UInt16?[,] frameData) { return; }
 
+        public static KinectSensor sensor = null;
+        public static DepthFrameReader depthFrameReader = null;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("hello, world");
+            Console.WriteLine("Hello, World!");
 
             // get the attached sensor
-            this.sensor = KinectSensor.GetDefault();
+            sensor = KinectSensor.GetDefault();
 
-            if (_sensor != null)
+            // get the depthFrameDescription from the DepthFrameSource
+            FrameDescription depthFrameDescription = sensor.DepthFrameSource.FrameDescription;
+
+            // open the reader for depth frames
+            depthFrameReader = sensor.DepthFrameSource.OpenReader();
+
+            // attach handler to be invoked upon frame arrival
+            depthFrameReader.FrameArrived += DepthFrameArrivedHandler;
+
+            // allocate space to put the pixels being received
+            // TODO: WTF IS THIS FOR?
+            ushort[] depthFrameData = new ushort[depthFrameDescription.Width * depthFrameDescription.Height];
+
+            // Open the sensor
+            sensor.Open();
+
+            // TODO: Implement IsAvailableChanged delegate
+            if (sensor.IsAvailable)
             {
                 Console.WriteLine("Sensor found!");
-
-                // get the depthFrameDescription from the DepthFrameSource
-                FrameDescription depthFrameDescription = this.sensor.DepthFrameSource.FrameDescription;
-
-                // open the reader for depth frames
-                this.depthFrameReader = this.sensor.DepthFrameSource.OpenReader();
-
-                // attach handler to be invoked upon frame arrival
-                this.depthFrameReader.FrameArrived += DepthFrameArrivedHandler;
-
-                // allocate space to put the pixels being received
-                this.depthFrameData = new ushort[depthFrameDescription.Width * depthFrameDescription.Height];
-                this.depthPixels = new byte[depthFrameDescription.Width * depthFrameDescription.Height * BytesPerPixel];
-
-                
-
             }
             else
             {
                 Console.WriteLine("Sensor not found");
             }
+
+            Console.ReadKey();
+
+            sensor.Close();
         }
 
+        private void Sensor_IsAvailableChanged(KinectSensor sender, IsAvailableChangedEventArgs args)
+        {
+            Console.WriteLine(sensor.IsAvailable ? "Running" : "Not Available");
+        }
+
+        // TODO: 
         private static void DepthFrameArrivedHandler(object sender, DepthFrameArrivedEventArgs e)
         {
+            Console.WriteLine("--- DEBUG: DepthFrameArrivedHandler() called.");
+            
+            // get the new depth frame and open it
+            using (DepthFrame depthFrame = e.FrameReference.AcquireFrame())
+            {
+                if (depthFrame != null)
+                {
+                    // get the frame
+                    FrameDescription depthFrameDescription = depthFrame.FrameDescription;
 
+                    // store the array to send to front end
+                    ushort[] frameData = new ushort[depthFrameDescription.Width * depthFrameDescription.Height];
+                    depthFrame.CopyFrameDataToArray(frameData);
+
+                    // Convert to 2D array
+                    int w = depthFrameDescription.Width;
+                    int h = depthFrameDescription.Height;
+                    UInt16?[,] frameData2D = new UInt16?[w, h];
+
+                    // Print frame data for debugging
+                    Console.WriteLine("Frame Data:");
+                    Console.WriteLine(frameData.ToString());
+
+                    for (int i = 0; i < w; i++)
+                    {
+                        for (int j = 0; j < h; j++)
+                        {
+                            // If data valid, write value, else write null
+                            UInt16 depth = frameData[i * w + j];
+                            if (depth <= depthFrame.DepthMaxReliableDistance && depth >= depthFrame.DepthMinReliableDistance)
+                            {
+                                frameData2D[i, j] = (UInt16) frameData[i * w + j];
+                            }
+                            else
+                            {
+                                frameData2D[i, j] = null;
+                            }
+                        }
+                    }
+
+                    // Give the frame to the front end
+                    Dummy(frameData2D);
+                }
+            }
         }
     }
 }
