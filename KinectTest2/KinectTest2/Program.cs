9b6ebe76203b49978e0c6dda031df88bae810a72
Ruhi <Ruhi>
2017-02-04 19:24:35 +0000

    detects kinect sensor

--- KinectTest2/KinectTest2/Program.cs
+++ KinectTest2/KinectTest2/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Kinect;
+using Microsoft.Kinect.Face;
+using Microsoft.Kinect.Fusion;
+using Microsoft.Kinect.Tools;
 
 namespace KinectTest2
 {
@@ -11,7 +14,48 @@ namespace KinectTest2
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("hello, world");
+            Console.WriteLine("hello, world");
+
+            // The sensor objects.
+            KinectSensor _sensor = null;
+
+            /*
+            // The color frame reader is used to display the RGB stream
+            ColorFrameReader _colorReader = null;
+
+            // The body frame reader is used to identify the bodies
+            BodyFrameReader _bodyReader = null;
+
+            // The list of bodies identified by the sensor
+            IList<Body> _bodies = null;
+
+            // The face frame source
+            FaceFrameSource _faceSource = null;
+
+            // The face frame reader
+            FaceFrameReader _faceReader = null;
+            */
+
+            // The depth sensor
+            DepthFrameReader _depthReader = null;
+
+
+            // finally, start
+            _sensor = KinectSensor.GetDefault();
+
+            if (_sensor != null)
+            {
+                _sensor.Open();
+
+                Console.WriteLine("Sensor found!");
+
+                // DO STUFF FINALLY
+
+            }
+            else
+            {
+                Console.WriteLine("Sensor not found");
+            }
         }
     }
 }
